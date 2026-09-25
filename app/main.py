370b0c111e748ba0d46bad6c1986b48bdf76a118
nkbta <hgggffdfaaa@gmail.com>
2026-09-25 18:13:43 +0530

    Remove applications router v2

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_url, "http://localhost:3000"],
+    allow_origins=[settings.frontend_url, "https://scheme-sarthi-nine.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
